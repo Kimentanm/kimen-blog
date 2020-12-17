75737a1e8cd8b96fc34841041783002cc1028d4b
Kimen Tang <kimentanm@gmail.com>
2020-12-17 10:45:58 +0800

    init

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,44 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '前端',
+    link: '/web/'
+  },
+  {
+    text: '后端',
+    link: '/ui/',
+  },
+  {
+    text: '技术',
+    link: '/technology/',
+    items: [
+      { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+      { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+    ],
+  },
+  {
+    text: '更多',
+    link: '/more/',
+  },
+  { text: '关于', link: '/about/' },
+  {
+    text: '收藏',
+    link: '/pages/beb6c0bd8a66cea6/',
+    items: [
+      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+    ],
+  },
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
+]
