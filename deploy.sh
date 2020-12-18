9f9c88182e3b7d11a68c23a68d394cb5cb8c8cbb
Kimen Tang <kimentanm@gmail.com>
2020-12-18 21:04:26 +0800

    新增gitee page自动化部署

--- deploy.sh
+++ deploy.sh
@@ -30,9 +30,9 @@ echo 'blog.kimen.com.cn' > CNAME  # 自定义域名
 echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 if [ -z "$GITEE_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  giteeUrl=git@gitee.com:Kimentanm/kimentanm.gitee.io.git
+  giteeUrl=git@gitee.com:Kimentanm/kimentanm.git
 else
-  giteeUrl=https://Kimentanm:${GITEE_TOKEN}@gitee.com/Kimentanm/kimentanm.gitee.io.git
+  giteeUrl=https://Kimentanm:${GITEE_TOKEN}@gitee.com/Kimentanm/kimentanm.git
 fi
 git add -A
 git commit -m "${msg}"
