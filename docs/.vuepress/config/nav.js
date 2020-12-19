ab0ef512ddc75bb70ccb0ee75a79ec3c912c56a8
Kimen Tang <kimentanm@gmail.com>
2020-12-19 16:39:54 +0800

    删除和增加一些博客
    (cherry picked from commit e49e555c5d2573a924ac347e00d4e188012287bf)

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -13,24 +13,21 @@ module.exports = [
     link: '/technology/',
     items: [
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-      { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+      { text: '经验之谈', link: '/pages/61e8fc/' },
+      { text: '疑难杂症', link: '/pages/117708e0af7f0bd9/' },
     ],
   },
   {
     text: '更多',
     link: '/more/',
+    items: [
+      { text: 'MAC软件分享', link: '/pages/809d08/' }
+    ],
   },
   { text: '关于', link: '/about/' },
   {
     text: '收藏',
     link: '/pages/beb6c0bd8a66cea6/',
-    items: [
-      { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-      { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    ],
   },
   {
     text: '索引',
