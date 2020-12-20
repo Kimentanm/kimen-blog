aebf35bed9f84e2d2e2d780f7932354e9aeb3a6b
Kimen Tang <kimentanm@gmail.com>
2020-12-20 21:42:56 +0800

    新增百度链接推送

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://xugaoyi.com&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.kimen.com.cn&token=CRZQz3dWrT6LnMi0"
 
 rm -rf urls.txt # 删除文件
